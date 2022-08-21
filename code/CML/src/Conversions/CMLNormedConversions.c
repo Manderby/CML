a600dfecb9b4f77994b5fca1ee7c4a3543ed7e92
Tobias Stamm <manderby@manderby.com>
2022-08-22 01:00:57 +0200

    Adding gamut slice function

--- code/CML/src/Conversions/CMLNormedConversions.c
+++ code/CML/src/Conversions/CMLNormedConversions.c
@@ -319,6 +319,335 @@ CML_DEF void cmlHSLToNormedCartesianOutput(CMLOutput output, CMLInput hsl, size_
 
 
 
+CMLOutput cmlCreateNormedGamutSlice(
+  CMLColorType colorSpace,
+  const size_t dimensions[CML_MAX_NUMBER_OF_CHANNELS],
+  const CMLVec4 origin,
+  const CMLVec4 range0,
+  const CMLVec4 range1,
+  const CMLVec4 range2,
+  const CMLVec4 range3){
+  
+  if(colorSpace > CML_COLOR_CMYK){return NULL;}
+  
+  size_t numChannels = cmlGetNumChannels(colorSpace);
+
+  CMLVec4 o;
+  CMLVec4 r[CML_MAX_NUMBER_OF_CHANNELS];
+  if(origin){cmlCpy4(o, origin);}else{cmlSet4(o, 0.f, 0.f, 0.f, 0.f);}
+  if(range0){cmlCpy4(r[0], range0);}else{cmlSet4(r[0], 1.f, 0.f, 0.f, 0.f);}
+  if(range1){cmlCpy4(r[1], range1);}else{cmlSet4(r[1], 0.f, 1.f, 0.f, 0.f);}
+  if(range2){cmlCpy4(r[2], range2);}else{cmlSet4(r[2], 0.f, 0.f, 1.f, 0.f);}
+  if(range3){cmlCpy4(r[3], range3);}else{cmlSet4(r[3], 0.f, 0.f, 0.f, 1.f);}
+  
+  size_t dim[CML_MAX_NUMBER_OF_CHANNELS];
+  CMLVec4 invDim;
+  size_t totalEntriesCount = 1;
+  int32_t dimCount = 0;
+  for(size_t d=0; d<CML_MAX_NUMBER_OF_CHANNELS; d++){
+    if(dimensions[d] > 1){
+      dim[dimCount] = dimensions[d];
+      totalEntriesCount *= dimensions[d];
+      invDim[dimCount] = 1.f / (dimensions[d] - 1);
+      if(dimCount != d){cmlCpy4(r[dimCount], r[d]);}
+      dimCount++;
+    }
+  }
+
+  float* array = (float*)malloc(totalEntriesCount * numChannels * sizeof(float));
+  float* ptr = array;
+  
+  CMLVec4 o1, o2, o3;
+
+  switch(numChannels){
+  case 1:
+    switch(dimCount){
+    case 0:
+      cmlCpy1(ptr, o);
+      break;
+    case 1:
+      for(size_t c0 = 0; c0 < dim[0]; ++c0){
+        float f0 = (float)c0 * invDim[0];
+        *ptr++ = o[0] + f0 * r[0][0];
+      }
+      break;
+    case 2:
+      for(size_t c1 = 0; c1 < dim[1]; ++c1){
+        float f1 = (float)c1 * invDim[1];
+        o1[0] = o[0] + f1 * r[1][0];
+        for(size_t c0 = 0; c0 < dim[0]; ++c0){
+          float f0 = (float)c0 * invDim[0];
+          *ptr++ = o1[0] + f0 * r[0][0];
+        }
+      }
+      break;
+    case 3:
+      for(size_t c2 = 0; c2 < dim[2]; ++c2){
+        float f2 = (float)c2 * invDim[2];
+        o2[0] = o[0] + f2 * r[2][0];
+        for(size_t c1 = 0; c1 < dim[1]; ++c1){
+          float f1 = (float)c1 * invDim[1];
+          o1[0] = o2[0] + f1 * r[1][0];
+          for(size_t c0 = 0; c0 < dim[0]; ++c0){
+            float f0 = (float)c0 * invDim[0];
+            *ptr++ = o1[0] + f0 * r[0][0];
+          }
+        }
+      }
+      break;
+    case 4:
+      for(size_t c3 = 0; c3 < dim[3]; ++c3){
+        float f3 = (float)c3 * invDim[3];
+        o3[0] = o[0] + f3 * r[3][0];
+        for(size_t c2 = 0; c2 < dim[2]; ++c2){
+          float f2 = (float)c2 * invDim[2];
+          o2[0] = o3[0] + f2 * r[2][0];
+          for(size_t c1 = 0; c1 < dim[1]; ++c1){
+            float f1 = (float)c1 * invDim[1];
+            o1[0] = o2[0] + f1 * r[1][0];
+            for(size_t c0 = 0; c0 < dim[0]; ++c0){
+              float f0 = (float)c0 * invDim[0];
+              *ptr++ = o1[0] + f0 * r[0][0];
+            }
+          }
+        }
+      }
+      break;
+    }
+    break;
+
+  case 2:
+    switch(dimCount){
+    case 0:
+      cmlCpy2(ptr, o);
+      break;
+    case 1:
+      for(size_t c0 = 0; c0 < dim[0]; ++c0){
+        float f0 = (float)c0 * invDim[0];
+        *ptr++ = o[0] + f0 * r[0][0];
+        *ptr++ = o[1] + f0 * r[0][1];
+      }
+      break;
+    case 2:
+      for(size_t c1 = 0; c1 < dim[1]; ++c1){
+        float f1 = (float)c1 * invDim[1];
+        o1[0] = o[0] + f1 * r[1][0];
+        o1[1] = o[1] + f1 * r[1][1];
+        for(size_t c0 = 0; c0 < dim[0]; ++c0){
+          float f0 = (float)c0 * invDim[0];
+          *ptr++ = o1[0] + f0 * r[0][0];
+          *ptr++ = o1[1] + f0 * r[0][1];
+        }
+      }
+      break;
+    case 3:
+      for(size_t c2 = 0; c2 < dim[2]; ++c2){
+        float f2 = (float)c2 * invDim[2];
+        o2[0] = o[0] + f2 * r[2][0];
+        o2[1] = o[1] + f2 * r[2][1];
+        for(size_t c1 = 0; c1 < dim[1]; ++c1){
+          float f1 = (float)c1 * invDim[1];
+          o1[0] = o2[0] + f1 * r[1][0];
+          o1[1] = o2[1] + f1 * r[1][1];
+          for(size_t c0 = 0; c0 < dim[0]; ++c0){
+            float f0 = (float)c0 * invDim[0];
+            *ptr++ = o1[0] + f0 * r[0][0];
+            *ptr++ = o1[1] + f0 * r[0][1];
+          }
+        }
+      }
+      break;
+    case 4:
+      for(size_t c3 = 0; c3 < dim[3]; ++c3){
+        float f3 = (float)c3 * invDim[3];
+        o3[0] = o[0] + f3 * r[3][0];
+        o3[1] = o[1] + f3 * r[3][1];
+        for(size_t c2 = 0; c2 < dim[2]; ++c2){
+          float f2 = (float)c2 * invDim[2];
+          o2[0] = o3[0] + f2 * r[2][0];
+          o2[1] = o3[1] + f2 * r[2][1];
+          for(size_t c1 = 0; c1 < dim[1]; ++c1){
+            float f1 = (float)c1 * invDim[1];
+            o1[0] = o2[0] + f1 * r[1][0];
+            o1[1] = o2[1] + f1 * r[1][1];
+            for(size_t c0 = 0; c0 < dim[0]; ++c0){
+              float f0 = (float)c0 * invDim[0];
+              *ptr++ = o1[0] + f0 * r[0][0];
+              *ptr++ = o1[1] + f0 * r[0][1];
+            }
+          }
+        }
+      }
+      break;
+    }
+    break;
+
+  case 3:
+    switch(dimCount){
+    case 0:
+      cmlCpy3(ptr, o);
+      break;
+    case 1:
+      for(size_t c0 = 0; c0 < dim[0]; ++c0){
+        float f0 = (float)c0 * invDim[0];
+        *ptr++ = o[0] + f0 * r[0][0];
+        *ptr++ = o[1] + f0 * r[0][1];
+        *ptr++ = o[2] + f0 * r[0][2];
+      }
+      break;
+    case 2:
+      for(size_t c1 = 0; c1 < dim[1]; ++c1){
+        float f1 = (float)c1 * invDim[1];
+        o1[0] = o[0] + f1 * r[1][0];
+        o1[1] = o[1] + f1 * r[1][1];
+        o1[2] = o[2] + f1 * r[1][2];
+        for(size_t c0 = 0; c0 < dim[0]; ++c0){
+          float f0 = (float)c0 * invDim[0];
+          *ptr++ = o1[0] + f0 * r[0][0];
+          *ptr++ = o1[1] + f0 * r[0][1];
+          *ptr++ = o1[2] + f0 * r[0][2];
+        }
+      }
+      break;
+    case 3:
+      for(size_t c2 = 0; c2 < dim[2]; ++c2){
+        float f2 = (float)c2 * invDim[2];
+        o2[0] = o[0] + f2 * r[2][0];
+        o2[1] = o[1] + f2 * r[2][1];
+        o2[2] = o[2] + f2 * r[2][2];
+        for(size_t c1 = 0; c1 < dim[1]; ++c1){
+          float f1 = (float)c1 * invDim[1];
+          o1[0] = o2[0] + f1 * r[1][0];
+          o1[1] = o2[1] + f1 * r[1][1];
+          o1[2] = o2[2] + f1 * r[1][2];
+          for(size_t c0 = 0; c0 < dim[0]; ++c0){
+            float f0 = (float)c0 * invDim[0];
+            *ptr++ = o1[0] + f0 * r[0][0];
+            *ptr++ = o1[1] + f0 * r[0][1];
+            *ptr++ = o1[2] + f0 * r[0][2];
+          }
+        }
+      }
+      break;
+    case 4:
+      for(size_t c3 = 0; c3 < dim[3]; ++c3){
+        float f3 = (float)c3 * invDim[3];
+        o3[0] = o[0] + f3 * r[3][0];
+        o3[1] = o[1] + f3 * r[3][1];
+        o3[2] = o[2] + f3 * r[3][2];
+        for(size_t c2 = 0; c2 < dim[2]; ++c2){
+          float f2 = (float)c2 * invDim[2];
+          o2[0] = o3[0] + f2 * r[2][0];
+          o2[1] = o3[1] + f2 * r[2][1];
+          o2[2] = o3[2] + f2 * r[2][2];
+          for(size_t c1 = 0; c1 < dim[1]; ++c1){
+            float f1 = (float)c1 * invDim[1];
+            o1[0] = o2[0] + f1 * r[1][0];
+            o1[1] = o2[1] + f1 * r[1][1];
+            o1[2] = o2[2] + f1 * r[1][2];
+            for(size_t c0 = 0; c0 < dim[0]; ++c0){
+              float f0 = (float)c0 * invDim[0];
+              *ptr++ = o1[0] + f0 * r[0][0];
+              *ptr++ = o1[1] + f0 * r[0][1];
+              *ptr++ = o1[2] + f0 * r[0][2];
+            }
+          }
+        }
+      }
+      break;
+    }
+    break;
+
+  case 4:
+    switch(dimCount){
+    case 0:
+      cmlCpy4(ptr, o);
+      break;
+    case 1:
+      for(size_t c0 = 0; c0 < dim[0]; ++c0){
+        float f0 = (float)c0 * invDim[0];
+        *ptr++ = o[0] + f0 * r[0][0];
+        *ptr++ = o[1] + f0 * r[0][1];
+        *ptr++ = o[2] + f0 * r[0][2];
+        *ptr++ = o[3] + f0 * r[0][3];
+      }
+      break;
+    case 2:
+      for(size_t c1 = 0; c1 < dim[1]; ++c1){
+        float f1 = (float)c1 * invDim[1];
+        o1[0] = o[0] + f1 * r[1][0];
+        o1[1] = o[1] + f1 * r[1][1];
+        o1[2] = o[2] + f1 * r[1][2];
+        o1[3] = o[3] + f1 * r[1][3];
+        for(size_t c0 = 0; c0 < dim[0]; ++c0){
+          float f0 = (float)c0 * invDim[0];
+          *ptr++ = o1[0] + f0 * r[0][0];
+          *ptr++ = o1[1] + f0 * r[0][1];
+          *ptr++ = o1[2] + f0 * r[0][2];
+          *ptr++ = o1[3] + f0 * r[0][3];
+        }
+      }
+      break;
+    case 3:
+      for(size_t c2 = 0; c2 < dim[2]; ++c2){
+        float f2 = (float)c2 * invDim[2];
+        o2[0] = o[0] + f2 * r[2][0];
+        o2[1] = o[1] + f2 * r[2][1];
+        o2[2] = o[2] + f2 * r[2][2];
+        o2[3] = o[3] + f2 * r[2][3];
+        for(size_t c1 = 0; c1 < dim[1]; ++c1){
+          float f1 = (float)c1 * invDim[1];
+          o1[0] = o2[0] + f1 * r[1][0];
+          o1[1] = o2[1] + f1 * r[1][1];
+          o1[2] = o2[2] + f1 * r[1][2];
+          o1[3] = o2[3] + f1 * r[1][3];
+          for(size_t c0 = 0; c0 < dim[0]; ++c0){
+            float f0 = (float)c0 * invDim[0];
+            *ptr++ = o1[0] + f0 * r[0][0];
+            *ptr++ = o1[1] + f0 * r[0][1];
+            *ptr++ = o1[2] + f0 * r[0][2];
+            *ptr++ = o1[3] + f0 * r[0][3];
+          }
+        }
+      }
+      break;
+    case 4:
+      for(size_t c3 = 0; c3 < dim[3]; ++c3){
+        float f3 = (float)c3 * invDim[3];
+        o3[0] = o[0] + f3 * r[3][0];
+        o3[1] = o[1] + f3 * r[3][1];
+        o3[2] = o[2] + f3 * r[3][2];
+        o3[3] = o[3] + f3 * r[3][3];
+        for(size_t c2 = 0; c2 < dim[2]; ++c2){
+          float f2 = (float)c2 * invDim[2];
+          o2[0] = o3[0] + f2 * r[2][0];
+          o2[1] = o3[1] + f2 * r[2][1];
+          o2[2] = o3[2] + f2 * r[2][2];
+          o2[3] = o3[3] + f2 * r[2][3];
+          for(size_t c1 = 0; c1 < dim[1]; ++c1){
+            float f1 = (float)c1 * invDim[1];
+            o1[0] = o2[0] + f1 * r[1][0];
+            o1[1] = o2[1] + f1 * r[1][1];
+            o1[2] = o2[2] + f1 * r[1][2];
+            o1[3] = o2[3] + f1 * r[1][3];
+            for(size_t c0 = 0; c0 < dim[0]; ++c0){
+              float f0 = (float)c0 * invDim[0];
+              *ptr++ = o1[0] + f0 * r[0][0];
+              *ptr++ = o1[1] + f0 * r[0][1];
+              *ptr++ = o1[2] + f0 * r[0][2];
+              *ptr++ = o1[3] + f0 * r[0][3];
+            }
+          }
+        }
+      }
+      break;
+    }
+    break;
+  }
+  
+  return array;
+}
+
 // This is free and unencumbered software released into the public domain.
 
 // Anyone is free to copy, modify, publish, use, compile, sell, or
